dde11b22cfd1af8f9dacdef81ac5c7c47724193e
Hamed <hameds@gmail.com>
2017-07-08 13:50:29 +0430

    initial commit

---
+++ Bugg.Ir.NLog/Bugg.Ir.NLog/ConfigManager.cs
@@ -0,0 +1,43 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Bugg.Ir.NLog
+{
+    public class ConfigManager
+    {
+        public static LoggingConfiguration GetBuggNLogConfig()
+        {
+            var config = new LoggingConfiguration();
+
+            var buggTarget = new WebServiceTarget();
+            config.AddTarget("bugg", buggTarget);
+            buggTarget.Url = new Uri("https://www.bugg.ir/api/log");
+            buggTarget.Protocol = WebServiceProtocol.HttpPost;
+            buggTarget.Encoding = Encoding.UTF8;
+
+            //Log Parameters for ASP.NET projects
+            foreach (var logParatmer in Model.NLogModel)
+            {                
+                buggTarget.Parameters.Add(new MethodCallParameter(logParatmer.Item1, logParatmer.Item2, typeof(System.String)));                
+            }
+
+            string buggAPIKey = ConfigurationManager.AppSettings["BuggAPIKey"]??"";
+            buggTarget.Parameters.Add(new MethodCallParameter("APIKeyId", buggAPIKey, typeof(System.String)));
+
+            string buggLevel= ConfigurationManager.AppSettings["BuggLevel"]??"Trace";
+            var logLevel = LogLevel.AllLevels.Where(c => c.Name.ToLower() == buggLevel.ToLower()).SingleOrDefault()??LogLevel.Trace;
+
+            var buggLogRule = new LoggingRule("*", logLevel, buggTarget);
+            config.LoggingRules.Add(buggLogRule);
+
+            return config;
+        }
+    }
+}
---
+++ Bugg.Ir.NLog/Bugg.Ir.NLog/readme.txt
@@ -0,0 +1,26 @@
+﻿--- What is bugg?
+Bugg is based on NLog (https://github.com/NLog/NLog) and sends your ASP.NET project logs to https://www.bugg.ir for better log search & analytics
+
+--- Configuration options
+after you install Bugg nuget package in your ASP.NET project, you will have two new appsetting keys in your web.config
+	- BuggAPIKey (Required) : your bugg.ir APIKey (read Get your APIKey section below)
+	- BuggLevel  (Required) : your log level, must be one of these values: Fatal, Error, Warn, Info, Debug, Trace
+
+--- Get your APIKey
+request APIKey from https://www.bugg.ir	
+
+--- How to start logging
+Bugg is based on NLog (https://github.com/NLog/NLog) so you can use NLog tutorial on logging (https://github.com/NLog/NLog/wiki/Tutorial)
+if you want to automatically log unhandled exceptions in your web application just add method below to your Global.asax file
+
+	protected void Application_Error()
+	{
+		Exception lastException = Server.GetLastError();
+		NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+		logger.Fatal(lastException);
+	}
+
+--- Contribute
+Bugg source code is available on github (https://github.com/Hameds/Bugg.Ir.Nlog)
+
+Made with ♥ in Iran  
